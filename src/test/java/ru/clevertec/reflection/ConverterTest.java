ee75288e27f32d48e9bb9bbe58fd6b0b370dd07a
TeaToJava <1234.testmode@gmail.com>
2024-09-22 22:11:58 +0300

    Add basic feature

---
+++ src/test/java/ru/clevertec/reflection/ConverterTest.java
@@ -0,0 +1,39 @@
+package ru.clevertec.reflection;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import ru.clevertec.model.Order;
+import ru.clevertec.model.Product;
+import ru.clevertec.reflection.utils.Converter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ConverterTest {
+
+    @Test
+    void createJson() throws IllegalAccessException {
+        Order order = new Order();
+        order.setId(UUID.randomUUID());
+        order.setCreateDate(OffsetDateTime
+                .of(LocalDate.of(2022, 03, 04).atStartOfDay(),
+                        ZoneId.of("Europe/Budapest").getRules().getOffset(LocalDateTime.now())));
+        Product product = new Product();
+        product.setName("U");
+        product.setQuantityInStocks(Map.of(UUID.randomUUID(), BigDecimal.valueOf(1214124142)));
+        Product product2 = new Product();
+        product2.setName("U2");
+        order.setProducts(List.of(product, product2));
+        String actual = Converter.formatJson(Converter.createJson(order));
+        String expected = new JSONObject(order).toString();
+        assertEquals(expected, actual);
+    }
+}
